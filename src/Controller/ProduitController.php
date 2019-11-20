2f11e09176e463c89a8ba68cfd0b79ed4c044a27
Aqua6 <46966135+TheAqua6@users.noreply.github.com>
2019-11-20 10:00:26 +0100

    Add files via upload

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProduitController extends AbstractController
+{
+    /**
+     * @Route("/", name="produits")
+     */
+    public function index()
+    {
+        $repository = $this->getDoctrine()->getRepository(Produit::class);
+
+        $produits = $repository->findAll();
+
+        return $this->render('index.html.twig', [
+            "produits"=>$produits
+        ]);
+    }
+
+    /**
+     * @Route("/produits/ajouter",name="produit_ajouter")
+     */
+    public function ajouter(Request $request)
+    {
+        $produit = new Produit();
+
+        $formulaire = $this->createForm(ProduitType::class, $produit);
+
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+
+            $em->persist($produit);
+
+            $em->flush();
+
+
+            return $this->redirectToRoute("produits");
+        }
+
+        return $this->render('formulaire.html.twig', [
+            "formulaire" => $formulaire->createView()
+            , "h1" => "Ajouter un produit"
+        ]);
+    }
+
+    /**
+     * @Route("/produits/modifier/{id}",name="produit_modifier")
+     */
+    public function modifier(Request $request, $id)
+    {
+
+        $repository=$this->getDoctrine()->getRepository(Produit::class);
+        $produit=$repository->find($id);
+
+
+        $formulaire = $this->createForm(ProduitType::class, $produit);
+
+
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+
+
+            $em->persist($produit);
+
+            $em->flush();
+
+
+            return $this->redirectToRoute("produits");
+        }
+
+        return $this->render('formulaire.html.twig', [
+            "formulaire" => $formulaire->createView()
+            , "h1" => "Modifier le produit ".$produit->getNom()
+        ]);
+    }
+}
